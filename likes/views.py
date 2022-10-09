e18ce8aa59eb17d1ea3fa988f5ebc1a7f60cd362
Kollecollier <kristoffer.collier@live.se>
2022-10-09 15:04:24 +0000

    added like view and prevent so user cant like same post twice, added like for for logged in and logged out user

---
+++ likes/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics, permissions
+from p5backend.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
+
+
+class LikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
