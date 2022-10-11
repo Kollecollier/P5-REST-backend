ef0286530a644fc4a7987cda897fa11ae9d76bba
Kollecollier <kristoffer.collier@live.se>
2022-10-11 15:25:11 +0000

    add dj-rest-auth, add django-filter

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions
 from p5backend.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 
@@ -11,6 +12,14 @@ class CommentList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+        filter_backends = [
+            DjangoFilterBackend,
+        ]
+
+        filterset_fields = [
+            'post'
+        ]
 
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
