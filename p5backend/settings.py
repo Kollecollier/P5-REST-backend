4b6283db60b2eadf3bee0e228394b9b668318150
Kollecollier <kristoffer.collier@live.se>
2022-10-27 13:03:40 +0000

    error fixing

--- p5backend/settings.py
+++ p5backend/settings.py
@@ -58,7 +58,7 @@ REST_AUTH_SERIALIZERS = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
